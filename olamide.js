b6b3417fb2dfcda841f740f18fb46a27742b9415
Atijani10 <abideen.tijani10@gmail.com>
2025-06-26 15:40:01 -0500

    first commit

---
+++ olamide.js
@@ -0,0 +1,11 @@
+const day = "friday"
+
+console.log (day)
+
+const position = "5th"
+
+console.log (position)
+
+const output = "Today is " + day + ". It is the "+ position + " day of the week."
+
+console.log (output)
